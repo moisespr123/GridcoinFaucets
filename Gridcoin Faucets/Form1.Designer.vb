2b81648fbd2d5a79ad5b928b68e25056b0a7003c
Moises Cardona <moisesmcardona@gmail.com>
2017-03-15 21:13:44 -0700

    Add project files.

---
+++ Gridcoin Faucets/Form1.Designer.vb
@@ -0,0 +1,247 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GridcoinAsiaButton = New System.Windows.Forms.Button()
+        Me.GridcoinAsiaText = New System.Windows.Forms.Label()
+        Me.EOBOTText = New System.Windows.Forms.Label()
+        Me.GridcoinStatsText = New System.Windows.Forms.Label()
+        Me.EOBOTFaucetButton = New System.Windows.Forms.Button()
+        Me.GridcoinStatsButton = New System.Windows.Forms.Button()
+        Me.GridcoinAsiaTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.EOBOTFaucetTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.GridcoinStatsTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.TrayUpdateTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 11)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(110, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Select a Faucet:"
+        '
+        'GridcoinAsiaButton
+        '
+        Me.GridcoinAsiaButton.Location = New System.Drawing.Point(16, 31)
+        Me.GridcoinAsiaButton.Margin = New System.Windows.Forms.Padding(4)
+        Me.GridcoinAsiaButton.Name = "GridcoinAsiaButton"
+        Me.GridcoinAsiaButton.Size = New System.Drawing.Size(169, 44)
+        Me.GridcoinAsiaButton.TabIndex = 1
+        Me.GridcoinAsiaButton.Text = "Gridcoin.asia Faucet"
+        Me.GridcoinAsiaButton.UseVisualStyleBackColor = True
+        '
+        'GridcoinAsiaText
+        '
+        Me.GridcoinAsiaText.AutoSize = True
+        Me.GridcoinAsiaText.ForeColor = System.Drawing.Color.Green
+        Me.GridcoinAsiaText.Location = New System.Drawing.Point(193, 46)
+        Me.GridcoinAsiaText.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.GridcoinAsiaText.Name = "GridcoinAsiaText"
+        Me.GridcoinAsiaText.Size = New System.Drawing.Size(188, 17)
+        Me.GridcoinAsiaText.TabIndex = 5
+        Me.GridcoinAsiaText.Text = "You can use this faucet now!"
+        '
+        'EOBOTText
+        '
+        Me.EOBOTText.AutoSize = True
+        Me.EOBOTText.ForeColor = System.Drawing.Color.Green
+        Me.EOBOTText.Location = New System.Drawing.Point(193, 97)
+        Me.EOBOTText.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.EOBOTText.Name = "EOBOTText"
+        Me.EOBOTText.Size = New System.Drawing.Size(188, 17)
+        Me.EOBOTText.TabIndex = 6
+        Me.EOBOTText.Text = "You can use this faucet now!"
+        '
+        'GridcoinStatsText
+        '
+        Me.GridcoinStatsText.AutoSize = True
+        Me.GridcoinStatsText.ForeColor = System.Drawing.Color.Green
+        Me.GridcoinStatsText.Location = New System.Drawing.Point(193, 149)
+        Me.GridcoinStatsText.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.GridcoinStatsText.Name = "GridcoinStatsText"
+        Me.GridcoinStatsText.Size = New System.Drawing.Size(188, 17)
+        Me.GridcoinStatsText.TabIndex = 7
+        Me.GridcoinStatsText.Text = "You can use this faucet now!"
+        '
+        'EOBOTFaucetButton
+        '
+        Me.EOBOTFaucetButton.Location = New System.Drawing.Point(16, 82)
+        Me.EOBOTFaucetButton.Margin = New System.Windows.Forms.Padding(4)
+        Me.EOBOTFaucetButton.Name = "EOBOTFaucetButton"
+        Me.EOBOTFaucetButton.Size = New System.Drawing.Size(169, 44)
+        Me.EOBOTFaucetButton.TabIndex = 9
+        Me.EOBOTFaucetButton.Text = "EOBOT Faucet"
+        Me.EOBOTFaucetButton.UseVisualStyleBackColor = True
+        '
+        'GridcoinStatsButton
+        '
+        Me.GridcoinStatsButton.Location = New System.Drawing.Point(16, 134)
+        Me.GridcoinStatsButton.Margin = New System.Windows.Forms.Padding(4)
+        Me.GridcoinStatsButton.Name = "GridcoinStatsButton"
+        Me.GridcoinStatsButton.Size = New System.Drawing.Size(169, 44)
+        Me.GridcoinStatsButton.TabIndex = 10
+        Me.GridcoinStatsButton.Text = "GridcoinStats Faucet"
+        Me.GridcoinStatsButton.UseVisualStyleBackColor = True
+        '
+        'GridcoinAsiaTimer
+        '
+        '
+        'EOBOTFaucetTimer
+        '
+        '
+        'GridcoinStatsTimer
+        '
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 221)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(473, 68)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "© 2016-2017 Moises Cardona" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Like this software? Consider donating some GRC to" &
+    " the following address:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(15, 344)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(457, 22)
+        Me.TextBox1.TabIndex = 13
+        Me.TextBox1.Text = "SAw6dAHTe2oZQTkLGCRjakB5pyHBu8hsAy"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(12, 236)
+        Me.LinkLabel1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(170, 17)
+        Me.LinkLabel1.TabIndex = 14
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "https://moisescardona.me"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(19, 186)
+        Me.CheckBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(183, 21)
+        Me.CheckBox1.TabIndex = 15
+        Me.CheckBox1.Text = "Minimize to System Tray"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(432, 221)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(35, 17)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "v1.1"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Location = New System.Drawing.Point(13, 253)
+        Me.LinkLabel2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(149, 17)
+        Me.LinkLabel2.TabIndex = 17
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "https://gridcoinapp.xyz"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(489, 414)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.GridcoinStatsButton)
+        Me.Controls.Add(Me.EOBOTFaucetButton)
+        Me.Controls.Add(Me.GridcoinStatsText)
+        Me.Controls.Add(Me.EOBOTText)
+        Me.Controls.Add(Me.GridcoinAsiaText)
+        Me.Controls.Add(Me.GridcoinAsiaButton)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Gridcoin Faucets"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GridcoinAsiaButton As Button
+    Friend WithEvents GridcoinAsiaText As Label
+    Friend WithEvents EOBOTText As Label
+    Friend WithEvents GridcoinStatsText As Label
+    Friend WithEvents EOBOTFaucetButton As Button
+    Friend WithEvents GridcoinStatsButton As Button
+    Friend WithEvents GridcoinAsiaTimer As Timer
+    Friend WithEvents EOBOTFaucetTimer As Timer
+    Friend WithEvents GridcoinStatsTimer As Timer
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents TrayUpdateTimer As Timer
+    Friend WithEvents Label7 As Label
+    Friend WithEvents LinkLabel2 As LinkLabel
+End Class
---
+++ Gridcoin Faucets/Form1.vb
@@ -0,0 +1,144 @@
+﻿Public Class Form1
+    Dim GridcoinAsiaReady As Integer = 1
+    Dim EOBOTReady As Integer = 1
+    Dim GridcoinStatsReady As Integer = 1
+    Dim GridcoinAsiaTimeToShow As TimeSpan
+    Dim GridcoinAsiaTotalTime As Long = TimeSpan.FromHours(6).TotalMilliseconds
+    Dim GridcoinAsiaRemaining As Long = GridcoinAsiaTotalTime
+    Dim EOBOTTimeToShow As TimeSpan
+    Dim EOBOTTotalTime As Long = TimeSpan.FromDays(1).TotalMilliseconds
+    Dim EOBOTRemaining As Long = EOBOTTotalTime
+    Dim GridcoinStatsTimeToShow As TimeSpan
+    Dim GridcoinStatsTotalTime As Long = TimeSpan.FromDays(1).TotalMilliseconds
+    Dim GridcoinStatsRemaining As Long = GridcoinStatsTotalTime
+    Private Sub GridcoinAsiaButton_Click(sender As Object, e As EventArgs) Handles GridcoinAsiaButton.Click
+        If GridcoinAsiaReady = 1 Then
+            GridcoinAsiaReady = 0
+            GridcoinAsiaTimer.Interval = 1000
+            GridcoinAsiaTimer.Enabled = True
+            GridcoinAsiaTimeToShow = TimeSpan.FromMilliseconds(GridcoinAsiaRemaining)
+            Process.Start("http://faucet.gridcoin.asia")
+        Else
+            Dim Result As DialogResult = MessageBox.Show("You can use this faucet in " & GridcoinAsiaTimeToShow.Hours.ToString.PadLeft(2, "0"c) & ":" & GridcoinAsiaTimeToShow.Minutes.ToString.PadLeft(2, "0"c) & ":" & GridcoinAsiaTimeToShow.Seconds.ToString.PadLeft(2, "0"c) & vbCrLf & "Do you want to visit this faucet anyway?", "Gridcoin Faucets", MessageBoxButtons.YesNo)
+            If Result = DialogResult.Yes Then
+                Process.Start("http://faucet.gridcoin.asia")
+            End If
+        End If
+    End Sub
+    Private Sub GridcoinAsiaTimer_Tick(sender As Object, e As EventArgs) Handles GridcoinAsiaTimer.Tick
+        If GridcoinAsiaRemaining = 0 Then
+            GridcoinAsiaRemaining = GridcoinAsiaTotalTime
+            GridcoinAsiaReady = 1
+            GridcoinAsiaText.ForeColor = Color.Green
+            GridcoinAsiaText.Text = "You can use this faucet now!"
+            NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+            NotifyIcon1.BalloonTipTitle = "Gridcoin Faucets"
+            NotifyIcon1.BalloonTipText = "You can now use Gridcoin Asia Faucet!"
+            NotifyIcon1.ShowBalloonTip(10000)
+            GridcoinAsiaTimer.Stop()
+        ElseIf GridcoinAsiaRemaining > 0 Then
+            GridcoinAsiaRemaining = GridcoinAsiaRemaining - TimeSpan.FromSeconds(1).TotalMilliseconds
+            GridcoinAsiaTimeToShow = TimeSpan.FromMilliseconds(GridcoinAsiaRemaining)
+            GridcoinAsiaText.ForeColor = Color.Red
+            GridcoinAsiaText.Text = "You can use this faucet again in " & GridcoinAsiaTimeToShow.Hours.ToString.PadLeft(2, "0"c) & ":" & GridcoinAsiaTimeToShow.Minutes.ToString.PadLeft(2, "0"c) & ":" & GridcoinAsiaTimeToShow.Seconds.ToString.PadLeft(2, "0"c)
+        End If
+    End Sub
+
+    Private Sub EOBOTFaucetButton_Click(sender As Object, e As EventArgs) Handles EOBOTFaucetButton.Click
+        If EOBOTReady = 1 Then
+            EOBOTReady = 0
+            EOBOTFaucetTimer.Interval = 1000
+            EOBOTFaucetTimer.Enabled = True
+            EOBOTTimeToShow = TimeSpan.FromMilliseconds(EOBOTRemaining)
+            Process.Start("https://www.eobot.com/faucet")
+        Else
+            Dim Result As DialogResult = MessageBox.Show("You can use this faucet in " & EOBOTTimeToShow.Hours.ToString.PadLeft(2, "0"c) & ":" & EOBOTTimeToShow.Minutes.ToString.PadLeft(2, "0"c) & ":" & EOBOTTimeToShow.Seconds.ToString.PadLeft(2, "0"c) & vbCrLf & "Do you want to visit this faucet anyway?", "Gridcoin Faucets", MessageBoxButtons.YesNo)
+            If Result = DialogResult.Yes Then
+                Process.Start("https://www.eobot.com/faucet")
+            End If
+        End If
+    End Sub
+    Private Sub EOBOTFaucetTimer_Tick(sender As Object, e As EventArgs) Handles EOBOTFaucetTimer.Tick
+        If EOBOTRemaining = 0 Then
+            EOBOTRemaining = EOBOTTotalTime
+            EOBOTReady = 1
+            EOBOTText.ForeColor = Color.Green
+            EOBOTText.Text = "You can use this faucet now!"
+            NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+            NotifyIcon1.BalloonTipTitle = "Gridcoin Faucets"
+            NotifyIcon1.BalloonTipText = "You can now use EOBOT Faucet!"
+            NotifyIcon1.ShowBalloonTip(10000)
+            EOBOTFaucetTimer.Stop()
+        ElseIf EOBOTRemaining > 0 Then
+            EOBOTRemaining = EOBOTRemaining - TimeSpan.FromSeconds(1).TotalMilliseconds
+            EOBOTTimeToShow = TimeSpan.FromMilliseconds(EOBOTRemaining)
+            EOBOTText.ForeColor = Color.Red
+            EOBOTText.Text = "You can use this faucet again in " & EOBOTTimeToShow.Hours.ToString.PadLeft(2, "0"c) & ":" & EOBOTTimeToShow.Minutes.ToString.PadLeft(2, "0"c) & ":" & EOBOTTimeToShow.Seconds.ToString.PadLeft(2, "0"c)
+        End If
+    End Sub
+
+
+    Private Sub GridcoinStatsButton_Click(sender As Object, e As EventArgs) Handles GridcoinStatsButton.Click
+        If GridcoinStatsReady = 1 Then
+            GridcoinStatsReady = 0
+            GridcoinStatsTimer.Interval = 1000
+            GridcoinStatsTimer.Enabled = True
+            GridcoinStatsTimeToShow = TimeSpan.FromMilliseconds(GridcoinStatsRemaining)
+            Process.Start("https://gridcoinstats.eu/faucet.php")
+        Else
+            Dim Result As DialogResult = MessageBox.Show("You can use this faucet in " & GridcoinStatsTimeToShow.Hours.ToString.PadLeft(2, "0"c) & ":" & GridcoinStatsTimeToShow.Minutes.ToString.PadLeft(2, "0"c) & ":" & GridcoinStatsTimeToShow.Seconds.ToString.PadLeft(2, "0"c) & vbCrLf & "Do you want to visit this faucet anyway?", "Gridcoin Faucets", MessageBoxButtons.YesNo)
+            If Result = DialogResult.Yes Then
+                Process.Start("https://gridcoinstats.eu/faucet.php")
+            End If
+        End If
+    End Sub
+    Private Sub GridcoinStatsTimer_Tick(sender As Object, e As EventArgs) Handles GridcoinStatsTimer.Tick
+        If GridcoinStatsRemaining = 0 Then
+            GridcoinStatsRemaining = GridcoinStatsTotalTime
+            GridcoinStatsReady = 1
+            GridcoinStatsText.ForeColor = Color.Green
+            GridcoinStatsText.Text = "You can use this faucet now!"
+            NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+            NotifyIcon1.BalloonTipTitle = "Gridcoin Faucets"
+            NotifyIcon1.BalloonTipText = "You can now use Gridcoin Stats Faucet!"
+            NotifyIcon1.ShowBalloonTip(10000)
+            GridcoinStatsTimer.Stop()
+        ElseIf GridcoinStatsRemaining > 0 Then
+            GridcoinStatsRemaining = GridcoinStatsRemaining - TimeSpan.FromSeconds(1).TotalMilliseconds
+            GridcoinStatsTimeToShow = TimeSpan.FromMilliseconds(GridcoinStatsRemaining)
+            GridcoinStatsText.ForeColor = Color.Red
+            GridcoinStatsText.Text = "You can use this faucet again in " & GridcoinStatsTimeToShow.Hours.ToString.PadLeft(2, "0"c) & ":" & GridcoinStatsTimeToShow.Minutes.ToString.PadLeft(2, "0"c) & ":" & GridcoinStatsTimeToShow.Seconds.ToString.PadLeft(2, "0"c)
+        End If
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://moisescardona.me")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        NotifyIcon1.Visible = True
+        NotifyIcon1.Icon = My.Resources.favicon
+        NotifyIcon1.Text = "Gridcoin Faucets"
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            If CheckBox1.Checked = True Then
+                ShowInTaskbar = False
+            End If
+        Else
+            ShowInTaskbar = True
+        End If
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        ShowInTaskbar = True
+        Me.WindowState = FormWindowState.Normal
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Process.Start("https://gridcoinapp.xyz")
+    End Sub
+
+
+End Class
